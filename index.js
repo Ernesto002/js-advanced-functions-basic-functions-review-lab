1e6182ecc59f0f92a5ec9339194f61db3fa60f2f
ernesto <ernesto002@icloud.com>
2021-06-08 10:52:11 -0700

    commit tree 0154188d583d80b6b113169ca6df6ffe6e842025

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+function saturdayFun(activity = `roller-skate`){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(work = `go to the office`){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(param0 = "*"){
+    return function inner(param1 = "special"){
+        return `You are ${param0}${param1}${param0}!`
+    }
+}
+
+const Calculator = {
+    add:function(a,b){return a+b},
+    subtract:function(a,b){return a-b},
+    multiply:function(a,b){return a*b},
+    divide:function(a,b){return a/b},
+}
+
+function actionApplyer(num,functions){
+    let realNum = num
+    const numbers = functions.map(function(e){
+      realNum = e(realNum)
+      return realNum
+    })
+    if (num != 0){
+   console.log(`${num}, multiplied by 2, added to 1000 and then modulo 7 is ${numbers[2]}. Apply each function in the Array of functions on the given base (13) OR on the result of the use of the previous function to get this result`)
+    return numbers[2]
+   }else{
+   return num
+    }
+}
